c5fedff45ef16c5154637e79a03d6ca10e4dabcb
norikois <33947244+norikois@users.noreply.github.com>
2023-08-31 14:16:31 +0900

    Created
    Created

---
+++ KB11675_WpfApp1/Infrastructure/ObservableObject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace KB11675_WpfApp1.Infrastructure;
+internal class ObservableObject : INotifyPropertyChanged
+{
+#nullable enable
+    protected ObservableObject()
+    {
+
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] String? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+#nullable disable
+}
---
+++ KB11675_WpfApp1/Model/Prefecture.cs
@@ -0,0 +1,31 @@
+﻿using KB11675_WpfApp1.Infrastructure;
+using System;
+using System.Windows.Documents;
+
+namespace KB11675_WpfApp1.Model;
+internal class Prefecture : ObservableObject
+{
+    private String _prefectureCode = "";
+
+    public String PrefectureCode
+    {
+        get { return _prefectureCode; }
+        set
+        {
+            _prefectureCode = value;
+            OnPropertyChanged();
+        }
+    }
+
+    private String _prefectureName = "";
+
+    public String PrefectureName
+    {
+        get { return _prefectureName; }
+        set { _prefectureName = value; OnPropertyChanged(); }
+    }
+
+    public Prefecture()
+    {
+    }
+}
